9a9a1f428ad4ac5bda876e2696b291d73ca54497
Daniel Possamai <daniel.possamai20@gmail.com>
2025-02-27 13:07:14 -0300

    Aula sobre os tipos integrador mais especifio number

---
+++ tiposIntegrados/numeros.dart
@@ -0,0 +1,15 @@
+// void main(){
+//   int idade = 12;
+
+//   //em dar os tipos são objetos então é possivel acessar propriedades e metodos deles, igual a baixo onde acessamos o metodo isOdd para saber se o numero é impar;
+//   print(idade.isOdd);
+// }
+
+
+
+void main(){
+  double preco = 2.3;
+
+  print(preco);
+  print(preco.toInt());
+}
